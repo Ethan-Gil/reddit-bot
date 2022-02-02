a69cd2bb709e76daadf872a3bcf92bf3895374c5
Ethan-Gil <ethan.gil1@outlook.com>
2022-02-02 17:54:30 -0500

    PyDictionary was very slow, so I created a new file with a method that utilizes the Webster's Dictionary API to define words.

---
+++ reddit_bot/webdict_definitions.py
@@ -0,0 +1,35 @@
+import requests
+import config
+
+# TODO: Before commit: Create a config file to hide the api key
+api_url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/%s?key={config.api_key}'
+
+
+# Sends a request to the API, handles the response data, and prints the definition of the word
+def get_definition(given_word):
+    try:
+        response = requests.get(api_url % given_word, timeout=3)
+        data = response.json()  # Storing the response data, which is a list of dictionaries containing word meanings.
+
+        # I think part of the issue is that 'shortdef' is not in the look up
+        # print(data[0])
+        #
+        # if not data[0]['shortdef'][0]:
+        #     print("Test")
+
+        word_info = data[0]  # We will store the first dictionary into word_info
+        definition = word_info['shortdef'][0]  # Since we only want one definition, we will find the first element in the value list corresponding to the shortdef key
+
+        if definition:
+            return f'Webster\'s Dictionary Defines {given_word} as "{definition}"'
+
+        else:
+            return 'Definition not found'
+
+    # TODO:  More Specific Exception Handling
+    except IndexError as error:
+        # raise IndexError('Fee Fi Fo Fum, someone got the Index wrong!')
+        return 1
+
+    except requests.Timeout as error:
+        print(error)
