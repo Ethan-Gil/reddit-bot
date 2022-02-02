7f3dac5b2edf683260938f2c44b53c3e10c5d329
Ethan-Gil <ethan.gil1@outlook.com>
2022-02-02 14:45:00 -0500

    Added a simple file that contains a method to retrieve a word's definition and return it. This is using the PyDictionary library.

---
+++ pydict_definitions.py
@@ -0,0 +1,16 @@
+from PyDictionary import PyDictionary
+
+# Creating a dictionary instance
+dictionary = PyDictionary()
+
+
+def get_definition(word):
+    dictionary_meaning = dictionary.meaning(word)
+    # TODO: Exception handling for words not in the dictionary
+
+    # Converting the meaning list (a python dict) into a list and extracting the first definition
+    meaning_list = list(dictionary_meaning.values())[0]
+    definition = meaning_list[0]
+
+    return f'The word "{word}" is defined as {definition}'
+
